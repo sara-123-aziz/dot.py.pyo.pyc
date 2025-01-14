dd620149afade4203e8b97ebe76b83040862296f
sara-123-aziz <azizsara97@gmail.com>
2025-01-14 20:07:55 +0530

    Create pythonier.py

---
+++ pythonier.py
@@ -0,0 +1,9 @@
+git stages
+3 stages
+
+working area or working directory or working place
+staging area or indexing area or modified area
+committing area or local repository or save as (save)
+
+by default whenever i create a excel workbook sheet1
+by default whenever i create git repo, master branch is the default branch
